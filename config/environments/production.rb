03551ba1cb3cf4506e801471881cfe78fa469d36
Michael Marvick <mmarvick@gmail.com>
2014-06-06 03:20:58 -0700

    HACKY solution which will need to be fixed later -- but Galleria now works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ PersonalSite::Application.configure do
    config.assets.js_compressor = :uglifier
 #  config.assets.css_compressor = :sass
 
+   # THIS IS NOT GOOD! I NEED TO FIGURE OUT A WAY TO MAKE GALERIA WORK WITHOUT CHANGING config.assets.compile SETTING
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 #  config.assets.compile = false
 
