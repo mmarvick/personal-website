a3f1c68c5d5621055781f6d3fd12dae79ce18a57
Michael Marvick <mmarvick@gmail.com>
2014-06-06 03:14:22 -0700

    Modifying settings to see if this fixes asset issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,8 @@ PersonalSite::Application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
- # config.consider_all_requests_local       = false
- # config.action_controller.perform_caching = true
+# config.consider_all_requests_local       = false
+# config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -24,13 +24,13 @@ PersonalSite::Application.configure do
 
   # Compress JavaScripts and CSS.
   #config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+#  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+#  config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+#  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
