ffee726fbc7f333500becbc179d9ec73ccd934da
Michael Marvick <mmarvick@gmail.com>
2014-11-07 13:15:37 -0800

    Don't access database while precompiling

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module PersonalSite
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/vendor/assets/libs"
